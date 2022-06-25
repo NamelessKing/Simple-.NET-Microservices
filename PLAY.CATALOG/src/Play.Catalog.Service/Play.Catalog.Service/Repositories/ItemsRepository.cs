be55c046f5d1ea6246a802005899611533c976f4
Tanjin <t.habibur@bizeta.net>
2022-06-25 12:58:53 +0200

    first commit

---
+++ PLAY.CATALOG/src/Play.Catalog.Service/Play.Catalog.Service/Repositories/ItemsRepository.cs
@@ -0,0 +1,62 @@
+﻿using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Play.Catalog.Service.Repositories
+{
+
+    public class ItemsRepository : IItemsRepository
+    {
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> dbCollection;
+        private readonly FilterDefinitionBuilder<Item> filterDefinitionBuilder = Builders<Item>.Filter;
+
+        public ItemsRepository(IMongoDatabase database)
+        {
+            dbCollection = database.GetCollection<Item>(collectionName);
+        }
+
+        public async Task<IReadOnlyCollection<Item>> GetAllAsync()
+        {
+            return await dbCollection.Find(filterDefinitionBuilder.Empty).ToListAsync();
+        }
+
+        public async Task<Item> GetAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterDefinitionBuilder.Eq(e => e.Id, id);
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(Item item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            await dbCollection.InsertOneAsync(item);
+        }
+
+        public async Task UpdateAsync(Item item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            FilterDefinition<Item> filter = filterDefinitionBuilder.Eq(e => e.Id, item.Id);
+
+            await dbCollection.ReplaceOneAsync(filter, item);
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterDefinitionBuilder.Eq(e => e.Id, id);
+
+            await dbCollection.DeleteOneAsync(filter);
+        }
+
+    }
+}
---
+++ PLAY.CATALOG/src/Play.Catalog.Service/Play.Catalog.Service/Settings/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Play.Catalog.Service.Settings
+{
+    public class MongoDbSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }   
+    }
+}
