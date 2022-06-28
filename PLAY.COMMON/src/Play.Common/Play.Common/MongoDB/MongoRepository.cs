ca0ce033b8d2b49e8844cde638c33407e267d8a5
Tanjin <habibur.tanjin@gmail.com>
2022-06-28 13:45:31 +0200

    extracted common code in a nuget package to share with different microservices

--- PLAY.COMMON/src/Play.Common/Play.Common/MongoDB/MongoRepository.cs
+++ PLAY.COMMON/src/Play.Common/Play.Common/MongoDB/MongoRepository.cs
@@ -1,10 +1,10 @@
 ﻿using MongoDB.Driver;
-using Play.Catalog.Service.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace Play.Catalog.Service.Repositories
+namespace Play.Common.MongoDB
 {
 
     public class MongoRepository<T> : IRepository<T> where T : IEntity
@@ -23,9 +23,19 @@ namespace Play.Catalog.Service.Repositories
             return await dbCollection.Find(filterDefinitionBuilder.Empty).ToListAsync();
         }
 
+        public async Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter)
+        {
+            return await dbCollection.Find(filter).ToListAsync();
+        }
+
         public async Task<T> GetAsync(Guid id)
         {
             FilterDefinition<T> filter = filterDefinitionBuilder.Eq(e => e.Id, id);
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
+        {
             return await dbCollection.Find(filter).FirstOrDefaultAsync();
         }
 
